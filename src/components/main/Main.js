a87adaa9017abdaa3d2d37926babf90414a36441
Ilham Yusron <ilhamyon@gmail.com>
2020-12-29 01:12:54 +0800

    fetching data API randomuser

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,101 +1,124 @@
 import "./Main.css";
-import avatarcard from "../../assets/avatarcard.png";
+import React, { Component } from "react";
 
-const Main = () => {
-  return (
-    <main>
-      <div className="main__container">
-        {/* <!-- MAIN TITLE STARTS HERE --> */}
 
-        <div className="main__title">
-          <div className="main__greeting">
-            <h1>PERSONNEL LIST</h1>
-            <p>List of all personnels</p>
-          </div>
-        </div>
+class Main extends Component {
 
-        {/* <!-- MAIN TITLE ENDS HERE --> */}
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
 
-        {/* <!-- MAIN CARDS STARTS HERE --> */}
-        <div className="main__cards">
-          <div className="card">
-            <div className="card_inner">
-              <p className="text-primary-p">Personnel ID: <a className="active_link" href="#!">123456</a></p>
-              <span className="font-bold text-title"><i className="fa fa-ellipsis-h"></i></span>
-            </div>
-            <img width="145" className="avatar_card" src={avatarcard} alt="avatarcard" />
-            <div className="bio">
-              <p className="name lebel">Name</p>
-              <p>First Name Last Name</p>
-              <p className="telphone lebel">Telephone</p>
-              <p>Phone Number</p>
-              <p className="birthday lebel">Birthday</p>
-              <p>DD-MM</p>
-              <p className="email lebel">Email</p>
-              <p>Email Address</p>
-            </div>
-          </div>
+  componentDidMount() {
+      fetch("https://randomuser.me/api/?results=4")
+        .then(res => res.json())
+        .then(parsedJSON => parsedJSON.results.map(data => (
+          {
+            id: `${data.id.value}`,
+            firstName: `${data.name.first}`,
+            lastName: `${data.name.last}`,
+            telepon: `${data.phone}`,
+            birthday: `${data.dob.date}`,
+            email: `${data.email}`,
+            thumbnail: `${data.picture.large}`,
 
-          <div className="card">
-            <div className="card_inner">
-              <p className="text-primary-p">Personnel ID: <a className="active_link" href="#!">123456</a></p>
-              <span className="font-bold text-title"><i className="fa fa-ellipsis-h"></i></span>
-            </div>
-            <img width="145" className="avatar_card" src={avatarcard} alt="avatarcard" />
-            <div className="bio">
-              <p className="name lebel">Name</p>
-              <p>First Name Last Name</p>
-              <p className="telphone lebel">Telephone</p>
-              <p>Phone Number</p>
-              <p className="birthday lebel">Birthday</p>
-              <p>DD-MM</p>
-              <p className="email lebel">Email</p>
-              <p>Email Address</p>
-            </div>
-          </div>
+          }
+        )))
+        .then(items => this.setState({
+          items,
+          isLoaded: false
+        }))
+        .catch(error => console.log('parsing failed', error))
+    }
 
-          <div className="card">
-            <div className="card_inner">
-              <p className="text-primary-p">Personnel ID: <a className="active_link" href="#!">123456</a></p>
-              <span className="font-bold text-title"><i className="fa fa-ellipsis-h"></i></span>
+    render() {
+      const {items } = this.state;
+        return (
+          <main>
+          <div className="main__container">
+            {/* <!-- MAIN TITLE STARTS HERE --> */}
+    
+            <div className="main__title">
+              <div className="main__greeting">
+                <h1>PERSONNEL LIST</h1>
+                <p>List of all personnels</p>
+              </div>
             </div>
-            <img width="145" className="avatar_card" src={avatarcard} alt="avatarcard" />
-            <div className="bio">
-              <p className="name lebel">Name</p>
-              <p>First Name Last Name</p>
-              <p className="telphone lebel">Telephone</p>
-              <p>Phone Number</p>
-              <p className="birthday lebel">Birthday</p>
-              <p>DD-MM</p>
-              <p className="email lebel">Email</p>
-              <p>Email Address</p>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card_inner">
-              <p className="text-primary-p">Personnel ID: <a className="active_link" href="#!">123456</a></p>
-              <span className="font-bold text-title"><i className="fa fa-ellipsis-h"></i></span>
-            </div>
-            <img width="145" className="avatar_card" src={avatarcard} alt="avatarcard" />
-            <div className="bio">
-              <p className="name lebel">Name</p>
-              <p>First Name Last Name</p>
-              <p className="telphone lebel">Telephone</p>
-              <p>Phone Number</p>
-              <p className="birthday lebel">Birthday</p>
-              <p>DD-MM</p>
-              <p className="email lebel">Email</p>
-              <p>Email Address</p>
+            <div className="main__cards">
+              {
+                items.length > 0 ? items.map(item => {
+                const {id, firstName, lastName, telepon, birthday, email, thumbnail} = item;
+                return (
+                    <div className="card">
+                      <div className="card_inner">
+                        <p className="text-primary-p">Personnel ID: <a className="active_link" href="#!">{id}</a></p>
+                        <span className="font-bold text-title"><i className="fa fa-ellipsis-h"></i></span>
+                      </div>
+                      <img width="145" className="avatar_card" src={thumbnail} alt="avatarcard" />
+                      <div className="bio">
+                        <p className="name lebel">Name</p>
+                        <p>{firstName} {lastName}</p>
+                        <p className="telphone lebel">Telephone</p>
+                        <p>{telepon}</p>
+                        <p className="birthday lebel">Birthday</p>
+                        <p>{birthday}</p>
+                        <p className="email lebel">Email</p>
+                        <p>{email}</p>
+                      </div>
+                    </div>
+                  );
+                }) : null
+              }
             </div>
           </div>
-        </div>
-        {/* <!-- MAIN CARDS ENDS HERE --> */}
+          </main>
+        );
+
+    }
+  }
+
+// const Main = () => {
+//   const [dataPersonnel, setDataPersonnel] = useState([]);
+//   useEffect(() => {
+//     axios.get('https://randomuser.me/api/?results=28')
+//     .then(result => {
+//       console.log('data API', result.data);
+//       const responseAPI = result.data;
+
+//       setDataPersonnel(responseAPI.data)
+//     })
+//     .catch(err => {
+//       console.log('error: ', err);
+//     })
+//   }, [])
+//   const history = useHistory();
+//   return (
+//     <main>
+//       <div className="main__container">
+//         {/* <!-- MAIN TITLE STARTS HERE --> */}
+
+//         <div className="main__title">
+//           <div className="main__greeting">
+//             <h1>PERSONNEL LIST</h1>
+//             <p>List of all personnels</p>
+//           </div>
+//         </div>
+
+//         {/* <!-- MAIN TITLE ENDS HERE --> */}
+
+//         {/* <!-- MAIN CARDS STARTS HERE --> */}
+//         <div className="main__cards">
+//         <PersonnelList />
+//         <PersonnelList />
+//         </div>
+//         {/* <!-- MAIN CARDS ENDS HERE --> */}
 
 
-      </div>
-    </main>
-  );
-};
+//       </div>
+//     </main>
+//   );
+// };
 
 export default Main;
